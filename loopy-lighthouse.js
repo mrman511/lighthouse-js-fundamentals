a880f33f864b1f1f035bf9e007e0d6e3308748a8
Paul Bodner <bodner80@gmail.com>
2021-12-12 15:27:25 -0800

    loopylighthouse project from lighthouse labs prep

---
+++ loopy-lighthouse.js
@@ -0,0 +1,18 @@
+let number = 100;
+
+while (number <= 200){
+  //if a multiple of three or four
+  if (number % 3 == 0 || number % 4 ==0){
+    //if a multiple of 4
+    if(number % 3 !=0){
+      console.log("Lighthouse" + number)
+    }
+    //if a multiple of 3
+    else if (number % 4 !=0) {
+      console.log("Loopy" + number)
+    }
+    //if a multiple of 3 and a multiple of 4
+    else { console.log("LoopyLighthouse" + number) }
+  }
+  number++
+}
