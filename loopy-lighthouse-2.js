5ddc9e2521ead2cfbc19eb3a0b8742d2fa8d65b3
Paul Bodner <bodner80@gmail.com>
2021-12-14 14:50:59 -0800

    LoopyLighthouse array project lighthouse labs prep

---
+++ loopy-lighthouse-2.js
@@ -0,0 +1,18 @@
+function loopyLighthouse(range, multiples, words){
+  for (let i = range[0]; i<= range[1]; i++){
+    if (i % multiples[0] === 0 || i % multiples[1] ===0){
+      if (i % multiples[0] != 0){
+        console.log("Beacon");
+      }
+      else if (i % multiples[1] != 0){
+         console.log("Batty"); 
+      }
+      else {console.log("BattyBeacon")}
+    }
+    else {
+      console.log(i);
+    }
+  }
+}
+
+loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"]);
